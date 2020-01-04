f3202bd9ef178e868f064cc33690daa632396384
NickyLi <376514069@qq.com>
2020-01-04 15:48:39 +0800

    Populate the genre_list function to access the genres on the routes

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,7 +1,13 @@
+const async = require('async')
+const book  = require('../models/book')
 const genre = require('../models/genre')
 
-exports.genre_list = (req,res) => {
-    res.send('NOT IMPLEMENTED:genre list')
+exports.genre_list = (req,res,next) => {
+    genre.find()
+    .exec((err,list_genre) => {
+        if(err){return next(err)}
+        res.render('genre-list',{title:'Genre List',genre_list:list_genre})
+    })
 }
 
 exports.genre_detaile = (req,res) =>{
