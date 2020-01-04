6f9583828d2c3870cf89a810c222d624ffd1a96c
NickyLi <376514069@qq.com>
2020-01-04 17:00:26 +0800

    Populate the genre detailes function

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -10,8 +10,27 @@ exports.genre_list = (req,res,next) => {
     })
 }
 
-exports.genre_detaile = (req,res) =>{
-    res.send('NOT IMPLEMENTED:genre detail' + req.params.id)
+exports.genre_detaile = (req,res,next) =>{
+    async.parallel({
+        genre:function(callback) {
+            genre.findById(req.params.id)
+                .exec(callback)
+        },
+        genre_books:function(callback){
+            book.find({'genre':req.params.id})
+                .exec(callback)
+        }
+    },function(err,result) {
+        if(err) {return next(err)}
+        if(result.genre === null){
+            let err = new Error('Genre not found')
+            err.status = 404
+            return next(err)
+        }
+
+        //successful
+        res.render('genre-detail',{title:'Genre Detail',genre:result.genre,genre_books:result.genre_books})
+    })
 }
 
 exports.genre_create_get = (req,res) => {
